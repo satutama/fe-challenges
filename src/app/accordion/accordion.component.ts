c8bcc488a919b5626dbd78b7d52e605b907cc7b8
Satria Utama <satria.u.s@gmail.com>
2023-12-05 21:35:03 +0100

    add angular svg icon

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,9 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-accordion',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, MatButtonModule, AngularSvgIconModule],
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.scss'],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,25 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    HttpClientModule,
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+
+    AngularSvgIconModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
