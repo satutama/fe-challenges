4ca3f460fdcdac5e43fb261b271f936a84bf0872
Satria Utama <satria.u.s@gmail.com>
2023-12-05 20:43:08 +0100

    accordion background

---
+++ src/app/accordion/accordion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  standalone: true,
+  imports: [],
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss'],
+})
+export class AccordionComponent {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccordionComponent } from './accordion/accordion.component';
 import { QrCodeComponent } from './qr-code/qr-code.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'qr-code' },
   { path: 'qr-code', component: QrCodeComponent },
+  { path: 'accordion', component: AccordionComponent },
 ];
 
 @NgModule({
