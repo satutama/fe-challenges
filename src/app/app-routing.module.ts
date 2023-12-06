895376aa7eb8000cddf13e5a5a2bc7d374512a1e
Satria Utama <satria.u.s@gmail.com>
2023-12-06 18:54:54 +0100

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccordionComponent } from './accordion/accordion.component';
-import { QrCodeComponent } from './qr-code/qr-code.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'accordion' },
-  { path: 'qr-code', component: QrCodeComponent },
-  { path: 'accordion', component: AccordionComponent },
+  {
+    path: 'qr-code',
+    loadComponent: () =>
+      import('./qr-code/qr-code.component').then((mod) => mod.QrCodeComponent),
+  },
+  {
+    path: 'accordion',
+    loadComponent: () =>
+      import('./accordion/accordion.component').then(
+        (mod) => mod.AccordionComponent
+      ),
+  },
   { path: '**', redirectTo: 'accordion' },
 ];
 
