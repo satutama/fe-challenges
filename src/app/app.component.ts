cef1852a03c46b756ab923165f6089d6e35ba439
Satria Utama <satria.u.s@gmail.com>
2023-12-06 12:56:51 +0100

    update sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'test-gh';
+  @ViewChild('sidenav') sidenav!: MatDrawer;
+
+  public title = 'test-gh';
+  public opened = false;
+
+  public sidenavToggle() {
+    this.sidenav.toggle();
+    this.opened = !this.opened;
+  }
 }
