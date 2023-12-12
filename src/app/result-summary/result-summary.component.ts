2347022878e3d32e1d2ecf55f7289a10e96bb939
Satria Utama <satria.u.s@gmail.com>
2023-12-12 13:46:29 +0100

    refactor

--- src/app/result-summary/result-summary.component.ts
+++ src/app/result-summary/result-summary.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+const DATA = [
+  {
+    category: 'Reaction',
+    score: 80,
+    icon: './assets/images/icon-reaction.svg',
+  },
+  {
+    category: 'Memory',
+    score: 92,
+    icon: './assets/images/icon-memory.svg',
+  },
+  {
+    category: 'Verbal',
+    score: 61,
+    icon: './assets/images/icon-verbal.svg',
+  },
+  {
+    category: 'Visual',
+    score: 72,
+    icon: './assets/images/icon-visual.svg',
+  },
+];
 
 @Component({
   selector: 'app-result-summary',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './result-summary.component.html',
-  styleUrls: ['./result-summary.component.scss']
+  styleUrls: ['./result-summary.component.scss'],
 })
 export class ResultSummaryComponent {
-
+  public data = DATA;
 }
