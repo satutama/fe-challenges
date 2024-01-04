ad3daecd3f9a95c5feb0058e1f962f15479d7164
Satria Utama <satria.u.s@gmail.com>
2024-01-04 16:26:01 +0100

    change detection

--- src/app/challenges/ip-address-tracker/ip-address-tracker.component.ts
+++ src/app/challenges/ip-address-tracker/ip-address-tracker.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import * as Leaflet from 'leaflet';
 import { Observable, ReplaySubject, merge, take } from 'rxjs';
@@ -14,6 +14,7 @@ import {
   imports: [CommonModule, LeafletModule],
   templateUrl: './ip-address-tracker.component.html',
   styleUrls: ['./ip-address-tracker.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IpAddressTrackerComponent {
   public readonly ipAddress$: Observable<IPResponse>;
