cec5d8a012af88a4b01b0bef9f19fd88fb7085a2
Satria Utama <satria.u.s@gmail.com>
2024-01-11 15:45:26 +0100

    service search with mock

--- src/app/challenges/ip-address-tracker/ip-address-tracker.service.ts
+++ src/app/challenges/ip-address-tracker/ip-address-tracker.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { MOCK_API } from './mock';
+import { Observable, Subject, of } from 'rxjs';
+import { MOCK_API, OTHER_MOCK_API } from './mock';
 
 export interface IPResponse {
   as: {
@@ -29,15 +29,25 @@ export interface IPResponse {
   providedIn: 'root',
 })
 export class IPAddressTrackerService {
+  public IPDetails$: Observable<IPResponse>;
+  private IPDetailsSubject = new Subject<IPResponse>();
   // private apiKey = 'at_W59ackEMWkfq16RP8nPkOYFD2cTj6';
   // private url = `https://geo.ipify.org/api/v2/country,city?apiKey=${this.apiKey}`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.IPDetails$ = this.IPDetailsSubject.asObservable();
+    this.getIPDetails();
+  }
 
-  public getIPDetails(): Observable<IPResponse> {
-    return of(MOCK_API);
+  public getLocalIp() {
     // return this.http
     //   .get<IPResponse>(`${this.url}`)
     //   .pipe(catchError((error) => throwError(() => error)));
+
+    return of(MOCK_API);
+  }
+
+  public getIPDetails(domain?: string) {
+    this.IPDetailsSubject.next(OTHER_MOCK_API);
   }
 }
