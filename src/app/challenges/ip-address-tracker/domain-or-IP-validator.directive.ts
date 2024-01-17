20978ff1efea518a3472b39f575b20d20b804eaf
Satria Utama <satria.u.s@gmail.com>
2024-01-17 14:31:06 +0100

    update validator

--- src/app/challenges/ip-address-tracker/domain-or-IP-validator.directive.ts
+++ src/app/challenges/ip-address-tracker/domain-or-IP-validator.directive.ts
@@ -1,16 +1,15 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function domainOrIPValidator(): ValidatorFn {
-  const domainRe = new RegExp(
-    '^(?!.* .*)(?:[a-z0-9][a-z0-9-]{0,61}[a-z0-9].)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$'
-  );
+  const domainRe = new RegExp(/^[a-z0-9A-Z\-]+\.[a-z]{2,6}$/);
 
   const ipRe = new RegExp(
-    '^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+    '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
   );
 
   return (control: AbstractControl): ValidationErrors | null => {
-    const valid = domainRe.test(control.value) || ipRe.test(control.value);
+    const valid = domainRe.exec(control.value) || ipRe.exec(control.value);
+
     return valid ? null : { domainName: { value: control.value } };
   };
 }
