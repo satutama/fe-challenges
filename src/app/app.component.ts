5cece72c7e0380c0763e0ebb7f35cc3ec563cbad
Satria Utama <satria.u.s@gmail.com>
2024-01-22 13:24:00 +0100

    refactor sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule, NgClass } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { routes } from './app.routes';
@@ -27,15 +27,5 @@ import { routes } from './app.routes';
   ],
 })
 export class AppComponent {
-  @ViewChild('sidenav') sidenav!: MatDrawer;
-
-  public title = 'test-gh';
-  public opened = false;
-
-  public sidenavToggle() {
-    this.sidenav.toggle();
-    this.opened = !this.opened;
-  }
-
   public routes = [...routes].filter((route) => !!route.title);
 }
