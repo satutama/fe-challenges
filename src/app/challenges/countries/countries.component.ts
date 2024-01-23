db6815de914b76cc144033c6568d4ded11cb3beb
Satria Utama <satria.u.s@gmail.com>
2024-01-23 19:50:11 +0100

    added country component

--- src/app/challenges/countries/countries.component.ts
+++ src/app/challenges/countries/countries.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CountryComponent } from './country/country.component';
 
 @Component({
   selector: 'app-countries',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CountryComponent],
   templateUrl: './countries.component.html',
 })
 export class CountriesComponent {}
---
+++ src/app/challenges/countries/country/country.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-country',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './country.component.html',
+  styleUrls: ['./country.component.scss']
+})
+export class CountryComponent {
+
+}
