e6eccb9c087e86a8a7779c0625aec66c9c5aa9e5
Satria Utama <satria.u.s@gmail.com>
2024-01-23 19:07:54 +0100

    added header

--- src/app/challenges/countries/countries.component.ts
+++ src/app/challenges/countries/countries.component.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-countries',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
 })
-export class CountriesComponent {
-
-}
+export class CountriesComponent {}
