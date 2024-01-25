c283916e96b554fd9ecb02c3543a96191f5b62b5
Satria Utama <satria.u.s@gmail.com>
2024-01-25 13:56:25 +0100

    restructure

--- src/app/challenges/countries/countries.component.ts
+++ src/app/challenges/countries/countries.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { CountryComponent } from './country/country.component';
+import { CountryCardComponent } from './components/country-card/country-card.component';
+import { CountryComponent } from './components/country/country.component';
 
 @Component({
   selector: 'app-countries',
   standalone: true,
-  imports: [CommonModule, CountryComponent],
+  imports: [CommonModule, CountryComponent, CountryCardComponent],
   templateUrl: './countries.component.html',
 })
 export class CountriesComponent {}
