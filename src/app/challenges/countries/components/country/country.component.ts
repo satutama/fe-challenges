a58cace8404aa8b3132e21dc61d8e01a9f14c9f5
Satria Utama <satria.u.s@gmail.com>
2024-01-25 14:54:10 +0100

    refactor

--- src/app/challenges/countries/components/country/country.component.ts
+++ src/app/challenges/countries/components/country/country.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-country',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './country.component.html',
-  styleUrls: ['./country.component.scss']
+  styleUrls: ['./country.component.scss'],
 })
-export class CountryComponent {
-
-}
+export class CountryComponent {}
