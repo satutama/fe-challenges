660ffe73c9e13053716ed08abcb738a897e8fe73
Satria Utama <satria.u.s@gmail.com>
2024-01-25 13:49:01 +0100

    country card component

---
+++ src/app/challenges/countries/country-card/country-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-country-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './country-card.component.html',
+  styleUrls: ['./country-card.component.scss']
+})
+export class CountryCardComponent {
+
+}
