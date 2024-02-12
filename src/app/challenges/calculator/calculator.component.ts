728b6571089b66592c3b570f597570122e6a39fd
Satria Utama <satria.u.s@gmail.com>
2024-02-12 14:43:10 +0100

    theme switcher

--- src/app/challenges/calculator/calculator.component.ts
+++ src/app/challenges/calculator/calculator.component.ts
@@ -1,10 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: ['./calculator.component.scss'],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './calculator.component.html',
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  themeForm = new FormGroup({
+    theme: new FormControl('dark'),
+  });
+
+  public get theme() {
+    return this.themeForm.controls.theme.value;
+  }
+}
