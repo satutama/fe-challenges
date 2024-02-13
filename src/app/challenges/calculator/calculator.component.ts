703f7d60b68ce54d27b13502dd09af23c426fee4
Satria Utama <satria.u.s@gmail.com>
2024-02-13 15:25:34 +0100

    added basic calculation

--- src/app/challenges/calculator/calculator.component.ts
+++ src/app/challenges/calculator/calculator.component.ts
@@ -3,6 +3,16 @@ import { Component, OnDestroy, OnInit, effect, signal } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
+enum OPERATOR {
+  DELETE = 'delete',
+  ADD = '+',
+  SUBSTRACT = '-',
+  MULTIPLY = '*',
+  DIVIDE = '/',
+  EVALUATE = '=',
+  RESET = 'reset',
+}
+
 @Component({
   selector: 'app-calculator',
   standalone: true,
@@ -16,8 +26,14 @@ export class CalculatorComponent implements OnInit, OnDestroy {
     JSON.parse(window.localStorage.getItem('calculatorTheme') ?? '1')
   );
 
+  public firstInput = signal<number | null>(null);
+  public secondInput = signal<number | null>(null);
+  public calculationOperator = signal<OPERATOR | null>(null);
+  public memory = signal<string | null>(null);
+
   public calculationControl = new FormControl<null | number>(null);
 
+  public operator = OPERATOR;
   private readonly subscriptions = new Subscription();
 
   constructor() {
@@ -38,7 +54,7 @@ export class CalculatorComponent implements OnInit, OnDestroy {
     this.subscriptions.unsubscribe();
   }
 
-  public calculateNumber(number: number): void {
+  public appendNumber(number: number): void {
     let updatedValue;
 
     if (this.calculationControl.value) {
@@ -50,10 +66,85 @@ export class CalculatorComponent implements OnInit, OnDestroy {
     this.calculationControl.setValue(updatedValue);
   }
 
+  public applyOperator(operator: OPERATOR): void {
+    switch (operator) {
+      case OPERATOR.DELETE:
+        this.calculationControl.setValue(null);
+        break;
+      case OPERATOR.ADD:
+        this.calculationOperator.set(OPERATOR.ADD);
+        this.firstInput.set(this.calculationControl.value);
+        this.memory.set(`${this.firstInput()} + `);
+        this.calculationControl.setValue(null);
+        break;
+      case OPERATOR.SUBSTRACT:
+        this.calculationOperator.set(OPERATOR.SUBSTRACT);
+        this.firstInput.set(this.calculationControl.value);
+        this.memory.set(`${this.firstInput()} - `);
+        this.calculationControl.setValue(null);
+        break;
+      case OPERATOR.DIVIDE:
+        this.calculationOperator.set(OPERATOR.DIVIDE);
+        this.firstInput.set(this.calculationControl.value);
+        this.memory.set(`${this.firstInput()} / `);
+        this.calculationControl.setValue(null);
+        break;
+      case OPERATOR.MULTIPLY:
+        this.calculationOperator.set(OPERATOR.MULTIPLY);
+        this.firstInput.set(this.calculationControl.value);
+        this.memory.set(`${this.firstInput()} * `);
+        this.calculationControl.setValue(null);
+        break;
+      case OPERATOR.EVALUATE:
+        const firstInput = this.firstInput() ?? null;
+        const secondInput = this.calculationControl.value ?? null;
+        const calculationOperator = this.calculationOperator() ?? null;
+
+        if (firstInput && secondInput && calculationOperator) {
+          this.secondInput.set(this.calculationControl.value);
+          this.memory.set(
+            `${this.firstInput()} ${calculationOperator} ${
+              this.calculationControl.value
+            }`
+          );
+          const result = this.calculate(
+            firstInput,
+            secondInput,
+            calculationOperator
+          );
+          console.log(firstInput, secondInput, operator, result);
+
+          this.calculationControl.setValue(result);
+        }
+        break;
+      case OPERATOR.RESET:
+        this.calculationControl.setValue(null);
+        this.memory.set(null);
+    }
+  }
+
   public get theme() {
     return this.themeControl.value;
   }
 
+  private calculate(
+    firstInput: number,
+    secondInput: number,
+    operator: OPERATOR
+  ): number {
+    switch (operator) {
+      case OPERATOR.ADD:
+        return firstInput + secondInput;
+      case OPERATOR.MULTIPLY:
+        return firstInput * secondInput;
+      case OPERATOR.DIVIDE:
+        return firstInput / secondInput;
+      case OPERATOR.SUBSTRACT:
+        return firstInput - secondInput;
+      default:
+        return 0;
+    }
+  }
   private themeListener(): Subscription {
     return this.themeControl.valueChanges.subscribe((value) =>
       this.selectedTheme.set(value)
