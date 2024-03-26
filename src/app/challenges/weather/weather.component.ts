fdee253046cafc6d964b4443c03f2e7915dd6e7e
Satria Utama <satria.u.s@gmail.com>
2024-03-26 14:01:52 +0100

    added weather component

---
+++ src/app/challenges/weather/weather.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-weather',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent {
+
+}
