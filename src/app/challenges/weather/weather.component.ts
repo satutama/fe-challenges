b3adf6cb52fc377cc8f3471018f61cf4be348898
Satria Utama <satria.u.s@gmail.com>
2024-03-26 16:45:28 +0100

    conditional bg

--- src/app/challenges/weather/weather.component.ts
+++ src/app/challenges/weather/weather.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
 })
 export class WeatherComponent {
+  public isDay = true;
 
+  public bgURL = this.isDay
+    ? "bg-[url('/assets/images/weather/day.jpg')]"
+    : "bg-[url('/assets/images/weather/night.jpg')]";
 }
