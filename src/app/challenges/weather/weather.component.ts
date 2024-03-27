1d4b3c3bb6d82aadca6cd9568230534940c5da43
Satria Utama <satria.u.s@gmail.com>
2024-03-27 15:49:39 +0100

    add icons

--- src/app/challenges/weather/weather.component.ts
+++ src/app/challenges/weather/weather.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.scss'],
 })
