d29c7f38175f7a0c80ef0f203388c5943fc6ccbd
Satria Utama <satria.u.s@gmail.com>
2024-04-08 21:50:06 +0200

    added url and apikey

--- src/app/challenges/weather/weather.service.ts
+++ src/app/challenges/weather/weather.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  private apiKey = '5acff7859a5c4c119b3194755240804';
+  private url = `http://api.weatherapi.com/v1/current.json?key=${this.apiKey}`;
 
-  constructor() { }
+  constructor() {}
 }
